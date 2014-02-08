86d1a1e50a8b705d82eacb25d951039f715ca4bb
ngld <ngld@tproxy.de>
2014-02-08 02:21:12 +0100

    First release! Added LICENSE.

--- manager.py
+++ manager.py
@@ -1,3 +1,17 @@
+## Copyright 2014 ngld <ngld@tproxy.de>
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
 import sys
 
 
@@ -48,7 +62,7 @@ from ui.gogextract import Ui_Dialog as Ui_Gogextract
 from ui.select_list import Ui_Dialog as Ui_SelectList
 from fs2mod import ModInfo2
 
-VERSION = '0.1-alpha'
+VERSION = '0.1'
 
 main_win = None
 progress_win = None
