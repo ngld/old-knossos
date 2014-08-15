931ab080cc76a425d7ba20f92af5a38fd88331a9
ngld <ngld@tproxy.de>
2014-08-15 21:41:36 +0200

    Added a mirror check.

---
+++ mirror_check.py
@@ -0,0 +1,78 @@
+## Copyright 2014 ngld <ngld@tproxy.de>
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+from __future__ import absolute_import, print_function
+import logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(threadName)s:%(module)s.%(funcName)s: %(message)s')
+
+import sys
+import argparse
+import json
+import time
+from six.moves.urllib.request import urlopen, Request
+from six.moves.urllib.error import URLError
+
+from lib import util
+
+
+def check(url, meth='HEAD'):
+    try:
+        result = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'}, method=meth))
+        result.close()
+        return True
+    except URLError:
+        return False
+    except:
+        logging.exception('Failed to check url "%s"!', url)
+        return False
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('checksumfile', help='Repo configuration with checksums.')
+    parser.add_argument('reportfile', help='The file that will contain the generated report.')
+
+    args = parser.parse_args(args)
+
+    logging.info('Reading repo configuration...')
+    with open(args.checksumfile, 'r') as stream:
+        mods = json.load(stream)
+
+    del mods['#generated']
+
+    report = {}
+    for mod in mods.values():
+        logging.info('Checking mod "%s"...', mod['title'])
+
+        mrep = report[mod['title']] = {}
+
+        for urls, files in mod['files']:
+            for name in files:
+                if name not in mrep:
+                    mrep[name] = {}
+
+                for url in urls:
+                    mrep[name][url] = check(util.pjoin(url, name))
+
+    logging.info('Writing report...')
+    report['#generated'] = time.strftime('%d.%m.%Y %H:%M GMT', time.gmtime())
+
+    with open(args.reportfile, 'w') as stream:
+        json.dump(report, stream, separators=(',', ':'))
+
+    logging.info('Done')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
