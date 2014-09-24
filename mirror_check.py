003bfac23248574ed343b4a7c3b306ffe8aa30a7
ngld <ngld@tproxy.de>
2014-09-24 23:59:43 +0200

    Update the license comments.

--- mirror_check.py
+++ mirror_check.py
@@ -1,4 +1,5 @@
-## Copyright 2014 ngld <ngld@tproxy.de>
+#!/usr/bin/python
+## Copyright 2014 fs2mod-py authors, see NOTICE file
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
