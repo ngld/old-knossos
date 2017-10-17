8196bf294614e92e2e1906f1f3a97dfa5feb16ab
MjnMixael <mjn.other@gmail.com>
2017-10-17 01:56:33 -0500

    Add P tags to the mod title, because I'd like to be able to scroll the title left ONLY IF there is overflow. But I'll need JS for that.

--- html/templates/kn-mod.vue
+++ html/templates/kn-mod.vue
@@ -75,7 +75,7 @@ export default {
                 </div>
             </div>
             <div class="mod-progress"><div class="bar" :style="'width: ' + mod.progress + '%'"></div></div>
-            <div class="mod-title">{{ mod.title }} {{ mod.version }}</div>
+            <div class="mod-title"><p>{{ mod.title }} {{ mod.version }}</p></div>
         </div>
     </div>
 </template>
