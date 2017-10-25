3cd0cf462cf5dcd0c4e7fb4526aa81f289c38e50
ngld <ngld@tproxy.de>
2017-10-25 18:51:56 +0200

    Fix the youtube embed for BBCode.

--- html/js/bbparser.js
+++ html/js/bbparser.js
@@ -32,7 +32,7 @@ const parser = XBBCode.create({
     td: '<td>{content}</td>',
     code: '<pre>{content}</pre>',
     quote: '<blockquote>{content}</blockquote>',
-    yt: '<iframe width="640" height="385" allowfullscreen src="https://youtube-nocookie.com/embed/{content}?version=3&vq=hd720"></iframe>',
+    yt: '<iframe width="640" height="385" allowfullscreen src="https://www.youtube-nocookie.com/embed/{content}?version=3&vq=hd720"></iframe>',
     p3d: '<iframe width="640" height="385" allowfullscreen src="http://p3d.in/e/{content}"></iframe>'
 });
 
