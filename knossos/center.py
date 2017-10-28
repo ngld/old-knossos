1790bf662126ed98d7f9899522de2a4993170e9a
ngld <ngld@tproxy.de>
2017-10-28 10:29:10 +0200

    Switch to the next version.

--- knossos/center.py
+++ knossos/center.py
@@ -24,7 +24,7 @@ from .qt import QtCore # noqa
 
 # The version should follow the http://semver.org guidelines.
 # Only remove the -dev tag if you're making a release!
-VERSION = '0.6.0-dev'
+VERSION = '0.7.0-dev'
 UPDATE_LINK = 'https://dev.tproxy.de/knossos'
 INNOEXTRACT_LINK = 'https://dev.tproxy.de/knossos/innoextract.txt'
 DEBUG = os.getenv('KN_DEBUG', '0').strip() == '1'
