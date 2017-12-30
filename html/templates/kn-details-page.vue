f7359a543b8c192555c2b2f53e3e6a8b356a1270
ngld <ngld@tproxy.de>
2017-12-30 22:53:53 +0100

    Only display FSO Settings for installed mods

--- html/templates/kn-details-page.vue
+++ html/templates/kn-details-page.vue
@@ -185,7 +185,7 @@ export default {
                         <button v-for="tool in tools" @click="launchTool(tool)">Run {{ tool }}</button>
                         <button @click="uploadLog">Upload Debug Log</button>
                         <button v-if="mod.id !== 'FS2' && mod.status !== 'updating' && mod.installed" @click="install">Modify</button>
-                        <button v-if="mod.status !== 'updating' && (mod.type === 'mod' || mod.type == 'tc')" @click="showFsoSettings">FSO Settings</button>
+                        <button v-if="mod.status !== 'updating' && mod.installed && (mod.type === 'mod' || mod.type == 'tc')" @click="showFsoSettings">FSO Settings</button>
                         <button v-if="mod.id !== 'FS2' && mod.status !== 'updating' && !mod.dev_mode" @click="uninstallMod">Uninstall</button>
                         <button v-if="mod.id !== 'FS2' && !mod.dev_mode" @click="verifyIntegrity">Verify file integrity</button>
                     </div>
