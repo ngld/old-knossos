c4b7c5ea5544668c59ff381909968e4dedd38407
ngld <ngld@tproxy.de>
2018-04-12 13:03:53 +0200

    Fix lost scroll position when switching between views (#120)

--- html/templates/kn-scroll-container.vue
+++ html/templates/kn-scroll-container.vue
@@ -1,8 +1,16 @@
 <script>
-export default {};
+export default {
+    data: () => ({
+        scroll: 0
+    }),
+
+    activated() {
+        this.$refs.container.scrollTop = this.scroll;
+    }
+};
 </script>
 <template>
-	<div class="main-container scroll-style">
+	<div class="main-container scroll-style" ref="container" @scroll.stop="scroll = $refs.container.scrollTop">
 	    <div class="main-background"></div>
 	    <slot></slot>
 	    <div class="main-shadow-effect"></div>
