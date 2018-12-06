250277ab7ff0d063c22af2961d5dc77b18c150f4
jg18 <jg18@users.noreply.github.com>
2018-12-05 21:35:55 -0800

    Revise welcome page text

--- html/templates/kn-welcome-page.vue
+++ html/templates/kn-welcome-page.vue
@@ -116,14 +116,12 @@ export default {
 };
 </script>
 <template>
-    <div class="container main-notice">
+    <div class="container main-notice" id="welcome-page">
         <div v-if="step === 1">
             <h1>Welcome!</h1>
-            
-            <p>It looks like you started Knossos for the first time.</p>
-            <p>You need to select a directory where Knossos will store the game data (models, textures, etc.).</p>
-            
-            <p>We recommend...</p>
+
+            <p>Select a folder where Knossos will store the game data (models, textures, etc.).</p>
+
             <form class="form-horizontal">
                 <div class="input-group">
                     <input type="text" class="form-control" v-model="data_path">
@@ -133,7 +131,7 @@ export default {
                 </div>
                 <br>
             </form>
-            
+
             <p>
                 <button v-if="retail_searching" class="btn btn-primary" disabled>Please wait...</button>
                 <button v-else class="btn btn-primary" @click.prevent="finishFirst">Continue</button>
@@ -141,13 +139,10 @@ export default {
         </div>
 
         <div v-else-if="step === 2">
-            <h1>Great!</h1>
-
             <form class="form-horizontal">
-                <p>Just a few more steps...</p>
-
                 <div v-if="retail_found">
-                    <p>We found an existing Freespace 2 installation in the location below. Is this correct?</p>
+                    <h1>FreeSpace 2 Installation Found</h1>
+                    <p>We found an existing FreeSpace 2 installation in the location below. Is this correct?</p>
 
                     <p><strong>{{ retail_path }}</strong></p>
 
@@ -157,30 +152,36 @@ export default {
                     </p>
                 </div>
                 <div v-else>
+                    <h1>Install FreeSpace 2</h1>
                     <p>
-                        If you want to play Freespace 2 Mods (Games that are built on and modify Retail Freespace 2 data), you'll need
-                        to get Retail Freespace 2 installed. First, you'll need to own a copy. You can buy it here at
-                        <a href="https://www.gog.com/game/freespace_2" class="open-ext">gog.com</a> for just a few dollars.
+                        If you want to play FreeSpace 2 mods (fan-made campaigns), then you'll need a copy of retail FreeSpace 2.
                     </p>
 
-                    <p>Once you have purchased Freespace 2, just point us to the GOG Installer EXE you downloaded.</p>
-                
-                    <div class="input-group">
-                        <input type="text" class="form-control" v-model="installer_path">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" @click.prevent="selectInstaller">Browse...</button>
-                        </span>
-                    </div>
-                    <br>
-
-                    <p>OR...</p>
-
-                    <p>If you already own Freespace 2, just point us to the retail data files (usually C:\Games\Freespace2).</p>
+                    <h2>Already have FreeSpace 2 installed?</h2>
+                    <p>
+                        Then point us to the retail data files (usually C:\Games\FreeSpace2).
+                    </p>
 
                     <div class="input-group">
                         <input type="text" class="form-control" v-model="retail_path">
                         <span class="input-group-btn">
                             <button class="btn btn-default" @click.prevent="selectRetailFolder">Browse...</button>
+                        </span>
+                    </div>
+
+                    <h2>Get FreeSpace 2</h2>
+                    <p>
+                        You can buy it from
+                        <a href="https://www.gog.com/game/freespace_2" class="open-ext">GOG.com</a> for cheap.
+                    </p>
+
+                    <p>Then download the FreeSpace 2 installer EXE and point us to it.</p>
+                    <p>For example: C:\Program Files (x86)\GOG Galaxy\Games\Freespace 2\!Downloads\setup_freespace2_2.0.0.8.exe</p>
+
+                    <div class="input-group">
+                        <input type="text" class="form-control" v-model="installer_path">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" @click.prevent="selectInstaller">Browse...</button>
                         </span>
                     </div>
                     <br>
@@ -197,83 +198,88 @@ export default {
             <h1>Suit Up, Pilot!</h1>
 
             <p>
-                Everything's setup and you can start playing Freespace Open games right away. Head on over to the
-                <a href="#" @click.prevent="goToExplore">Explore</a> tab and pick a few to start with.
+                Now you can start playing FreeSpace Open games! You can find games on the
+                <a href="#" @click.prevent="goToExplore">Explore</a> tab.
             </p>
 
-            <p v-if="retail_installed">
-                If you are interested in playing Retail Freespace 2, we recommend you install the
-                <a href="#" @click.prevent="goToMVPS">MediaVPs mod</a>. That mod will let you play Freespace 2 with all
-                of the glittery graphical goodness that Hard-Light has created over the years!
-            </p>
+            <div v-if="retail_installed">
+                <h2>Play FreeSpace 2 with the MediaVPs</h2>
+                <p>
+                    If you want to play retail FreeSpace 2, we <strong>highly</strong> recommend you install the
+                    <a href="#" @click.prevent="goToMVPS">MediaVPs mod</a>, which greatly improves the graphics.
+                </p>
+            </div>
 
             <p><button class="btn btn-primary" @click.prevent="goToExplore">Finish</button></p>
 
             <hr>
 
-                <div class="welcome-login">
-                    Are you a mod developer? Login!
-                    <div class="login-form">
-                        <div class="form-group">
-                            <label class="col-sm-4 welcome-label">Username:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="neb-input" v-model="neb_user">
-                            </div>
+            <div class="welcome-login">
+                Are you a mod developer? Log in!
+                <div class="login-form">
+                    <div class="form-group">
+                        <label class="col-sm-4 welcome-label">Username:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="neb-input" v-model="neb_user">
                         </div>
+                    </div>
 
-                        <br>
+                    <br>
 
-                        <div class="form-group">
-                            <label class="col-sm-4 welcome-label">Password:</label>
-                            <div class="col-sm-8">
-                                <input type="password" class="neb-input" v-model="neb_password">
-                            </div>
+                    <div class="form-group">
+                        <label class="col-sm-4 welcome-label">Password:</label>
+                        <div class="col-sm-8">
+                            <input type="password" class="neb-input" v-model="neb_password">
                         </div>
+                    </div>
 
-                        <br>
+                    <br>
 
-                        <div class="form-group">
-                            <label class="col-sm-4 welcome-label">E-Mail:</label>
-                            <div class="col-sm-8">
-                                <input type="email" class="neb-input" v-model="neb_email" placeholder="only required for registration">
-                            </div>
+                    <div class="form-group">
+                        <label class="col-sm-4 welcome-label">E-Mail:</label>
+                        <div class="col-sm-8">
+                            <input type="email" class="neb-input" v-model="neb_email" placeholder="only required for registration">
                         </div>
+                    </div>
 
-                        <br>
+                    <br>
 
-                        <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-8 neb-btns">
-                                <button class="mod-btn btn-link-blue" @click="login">Login</button>
-                                <button class="mod-btn btn-link-blue" @click="register">Register</button>
-                                <button class="mod-btn btn-link-red" @click="resetPassword">Reset Pass</button>
-                            </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-8 neb-btns">
+                            <button class="mod-btn btn-link-blue" @click="login">Login</button>
+                            <button class="mod-btn btn-link-blue" @click="register">Register</button>
+                            <button class="mod-btn btn-link-red" @click="resetPassword">Reset Password</button>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
 
         <div v-else>
-            <h1>Whoops</h1>
+            <h1>Whoops!</h1>
 
-            <p>You somehow managed to end up in nirvana. If you see this page, please tell us how you managed that!</p>
+            <p>If you see this page, please tell us how you managed to get here!</p>
         </div>
 
         <hr>
-
-        <p>
-            This launcher is still in development. Join us on
-            <a href="https://discord.gg/qfReB8t" class="open-ext">#knossos</a>
-            in HLP's Discord or visit our
-            <a href="https://www.hard-light.net/forums/index.php?topic=94068.0" class="open-ext">release thread</a>
-            and let us know what you think, what didn't work and what you would like to change.
-        </p>
-        <p>-- ngld</p>
-
+        <div id="welcome-footer">
+            <h2>Need help or have feedback?</h2>
+            <p>
+                Join the <a href="https://discord.gg/qfReB8t" class="open-ext">#knossos</a> channel on Discord or post
+                on the <a href="https://www.hard-light.net/forums/index.php?topic=94068.0" class="open-ext">Knossos release thread</a> and tell us
+            </p>
+            <ul>
+                <li>What do you like about Knossos?</li>
+                <li>What problems did you run into?</li>
+                <li>What changes do you want to see?</li>
+            </ul>
+            <p>-- ngld</p>
+        </div>
         <div class="popup-bg" v-if="wl_popup_visible" @click.prevent="wl_popup_visible = false"></div>
 
         <div class="popup" v-if="wl_popup_visible">
             <div class="title clearfix">
-                Are You Sure?
+                Skip FreeSpace 2 Installation?
 
                 <a href="" class="pull-right" @click.prevent="wl_popup_visible = false">
                     <i class="fa fa-times"></i>
@@ -281,11 +287,9 @@ export default {
             </div>
             <div class="content gen-scroll-style">
                 <p>
-                    You can skip this step, but you won't be able to play most games that Knossos offers.
-                    If you try to install a Freespace2 mod, you'll be asked to install Retail Freespace2 again.
+                    Without FreeSpace 2, you won't be able to play most games that Knossos offers.
                 </p>
-
-                <p>You can always install Retail Freespace2 from the settings!</p>
+                <p>You can install FreeSpace 2 at any time from the Settings.</p>
 
                 <div class="popup-buttons">
                     <button class="btn btn-default" @click.prevent="skipRetail">Skip</button>
