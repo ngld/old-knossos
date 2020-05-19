253d544511148d4fe38b981263aa66b473bde0f3
Cliff Gordon <cliff.gordon@gatewayblend.com>
2020-05-19 15:31:07 -0500

    fix/mactravis_v3: Step 7: (Lucky 7s?) Since it apparently didn't load the PATH in the builder after the provisioning, let's add the yarn path manually like all the other ones we do here.

--- releng/macos/auto-build.sh
+++ releng/macos/auto-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export PATH="/usr/local/bin:$PATH:/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/opt/qt5/bin"
+export PATH="/usr/local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH:/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/opt/qt5/bin"
 export LANG="en_US.UTF-8"
 
 # ninja ends up in a reconfigure loop here without this
